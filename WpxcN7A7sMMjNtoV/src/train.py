ef51e2cc9d918e1d7f70417109eb4a87e78a2908
lchjoel1031 <lchjoel1031@gmail.com>
2023-10-22 18:32:38 -1000

    Create train.py

---
+++ WpxcN7A7sMMjNtoV/src/train.py
@@ -0,0 +1,22 @@
+import numpy as np
+import pandas as pd
+
+from sklearn.metrics.pairwise import cosine_similarity
+
+import os
+import warnings
+warnings.filterwarnings('ignore')
+
+data = '../data/potential-talents.xlsx'
+df = pd.read_excel(data)
+
+#Load BERT model
+from sentence_transformers import SentenceTransformer
+model = SentenceTransformer('bert-base-nli-mean-tokens')
+
+#Encoding:
+sen_embeddings = model.encode(df['job_title'])
+keyword_embeddings = model.encode(['Aspiring human resources'])
+similarity = cosine_similarity(sen_embeddings,keyword_embeddings)
+df['fit'] = similarity
+df.sort_values(by=['fit'],ascending=False).head()
