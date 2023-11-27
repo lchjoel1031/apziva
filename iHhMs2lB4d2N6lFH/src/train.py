34c52ca208a8b280084d5f086d4e377d005ee0f0
lchjoel1031 <lchjoel1031@gmail.com>
2023-11-26 22:19:29 -1000

    Create train.py

---
+++ iHhMs2lB4d2N6lFH/src/train.py
@@ -0,0 +1,104 @@
+import tensorflow as tf
+from tensorflow.keras import layers as tfl
+from tensorflow.keras.utils import image_dataset_from_directory
+from tensorflow.keras.initializers import glorot_uniform
+from keras import backend as K
+import matplotlib.pyplot as plt
+import os
+
+train_data_dir = './data/images/training'
+test_data_dir = './data//images/testing'
+model_dir = './models/'
+
+def f1_score(y_true, y_pred):
+    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+    possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
+    predicted_positives = K.sum(K.round(K.clip(y_pred, 0, 1)))
+    precision = true_positives / (predicted_positives + K.epsilon())
+    recall = true_positives / (possible_positives + K.epsilon())
+    f1_val = 2*(precision*recall)/(precision+recall+K.epsilon())
+    return f1_val
+
+
+#parameters
+BATCH_SIZE = 32
+IMG_SIZE = (128, 128)
+
+#train data
+train_dataset, validation_dataset = image_dataset_from_directory(train_data_dir,
+                                                                 shuffle=True,
+                                                                 validation_split = 0.2,
+                                                                 subset = 'both',
+                                                                 batch_size=BATCH_SIZE,
+                                                                 image_size=IMG_SIZE,
+                                                                 seed=42)
+#test data
+test_dataset = image_dataset_from_directory(test_data_dir,
+                                            shuffle=False,
+                                            image_size=IMG_SIZE,
+                                            seed=42)
+
+class_names = train_dataset.class_names
+AUTOTUNE = tf.data.experimental.AUTOTUNE
+train_dataset = train_dataset.prefetch(buffer_size=AUTOTUNE)
+
+tf.random.set_seed(42)
+
+preprocess_input = tf.keras.applications.mobilenet_v2.preprocess_input
+
+#define model
+input_shape = IMG_SIZE + (3,)
+data_augmentation = tf.identity
+base_model = tf.keras.applications.MobileNetV2(input_shape = input_shape, include_top = False, weights = 'imagenet')
+base_model.trainable = False
+
+inputs = tf.keras.Input(shape=input_shape)
+x = data_augmentation(inputs)
+x = preprocess_input(x)
+x = base_model(x, training=False)
+x = tfl.GlobalAveragePooling2D()(x)
+x = tfl.Dropout(0.2)(x)
+outputs = tfl.Dense(1, activation = 'linear')(x)
+
+mobilenet_model = tf.keras.Model(inputs, outputs)
+
+mobilenet_model.compile(optimizer = tf.keras.optimizers.Adam(learning_rate = 0.0005),
+                          loss = tf.keras.losses.BinaryCrossentropy(from_logits=True),
+                          metrics = ['accuracy', f1_score])
+
+#initial training
+initial_epochs = 3
+history = mobilenet_model.fit(train_dataset,
+                                validation_data=validation_dataset,
+                                epochs=initial_epochs)
+
+#finetuning
+fine_tune_at = 120
+for layer in base_model.layers[:fine_tune_at]:
+    layer.trainable = False
+
+loss_function = tf.keras.losses.BinaryCrossentropy(from_logits=True)
+lr_schedule = tf.keras.optimizers.schedules.ExponentialDecay(initial_learning_rate = 0.0001,
+                                                             decay_steps = len(train_dataset),
+                                                             decay_rate = 0.8,
+                                                             staircase = False
+                                                             )
+optimizer = tf.keras.optimizers.Adam(learning_rate = lr_schedule)
+metrics = ['accuracy', f1_score]
+
+mobilenet_model.compile(loss=loss_function,
+                          optimizer = optimizer,
+                          metrics=metrics
+                          )
+
+fine_tune_epochs = 3
+total_epochs =  initial_epochs + fine_tune_epochs
+
+history_fine = mobilenet_model.fit(train_dataset,
+                                     epochs=total_epochs,
+                                     initial_epoch=history.epoch[-1],
+                                     validation_data=validation_dataset
+                                     )
+
+#Save model
+mobilenet_model.save_weights(model_dir+'MobileNetV2')
